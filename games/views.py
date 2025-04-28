5a02f4cbc16ba0c58f434e56154567ea94c827fd
Josh Hanson <jhanson.44@gmail.com>
2025-04-28 13:11:37 +0100

    updated igdb bearer token

--- games/views.py
+++ games/views.py
@@ -336,7 +336,7 @@ def igdb_data(query_type, input):
     endpoint = 'https://api.igdb.com/v4'
     HEADERS = {
         'Client-ID': 'eclpixd8yx6t9lfnn52s84xkcpgyq0',
-        'Authorization': 'Bearer 18f8wq89w120cycm1yxjceffh2kcjx'
+        'Authorization': 'Bearer 93nj18cq31dwtd9ad7c3pxdf7d43no'
     }
 
     if query_type == 'search':
