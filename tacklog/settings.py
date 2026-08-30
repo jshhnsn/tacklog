90c79e84b5b5f9294bb6509d52a0762f0f4b4c79
Josh Hanson <jhanson.44@gmail.com>
2026-08-30 18:46:08 +0100

    added csrf trusted hosts

--- tacklog/settings.py
+++ tacklog/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
-
+CSRF_TRUSTED_ORIGINS = os.environ.get("CSRF_TRUSTED_ORIGINS", "").split(",")
 
 # Application definition
 
